735eb8a4a44594c5d0e929c6802706c5f8e1879c
meetkathiriya1320 <meet.kathiriya1320@gmail.com>
2026-02-06 15:30:36 +0530

    first commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://selection-be-1.onrender.com/api', // TODO: Move to env var
+    baseURL: `${import.meta.env.VITE_API_URL}/api`,
     headers: {
         'Content-Type': 'application/json',
     },
